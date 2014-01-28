f6aa4e8f7f3c4d4875ffb6a1ad3e99151d40f43c
sreshetnyak <reshetniak.sergey@gmail.com>
2014-01-28 19:41:22 +0200

    Initial commit from local git

---
+++ Dictionary/TTViewController.m
@@ -0,0 +1,38 @@
+//
+//  TTViewController.m
+//  Dictionary
+//
+//  Created by sergey on 1/28/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import "TTViewController.h"
+
+@interface TTViewController ()
+
+@end
+
+@implementation TTViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
